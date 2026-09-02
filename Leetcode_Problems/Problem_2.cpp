2b1553a0399f41f11867190cada67460ccf2eaa0
krutikbarikar285-byte <krutikbarikar285@gmail.com>
2026-09-02 22:54:28 +0530

    Add files via upload

---
+++ Leetcode_Problems/Problem_2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the Value of N:"<<endl;
+    cin>>n;
+    int count=0;
+    while(n!=0){
+        if(n&1){
+            count++;
+        }
+        n=n>>1;
+    }
+    cout<<count;
+}
